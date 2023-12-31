233e2a3b8483a04b70d051f5db6d8fb5f1de5793
Adam Czerniak <adam11392@gmail.com>
2023-12-31 17:39:47 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+const Home = () => {
+  return <h1>Strona startowa</h1>;
+};
+
+const News = () => {
+  return <h1>Aktualności</h1>;
+};
+
+const Contact = () => {
+  return <h1>Kontakt</h1>;
+};
+
+const ErrorPage = () => {
+  return <h1>Strona nie istnieje</h1>;
+};
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <Router>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                      isActive ? "home_selected" : ""
+                    }
+                  >
+                    Start
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/news"
+                    className={({ isActive }) =>
+                      isActive ? "news_selected" : ""
+                    }
+                  >
+                    Aktualności
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/contact"
+                    className={({ isActive }) =>
+                      isActive ? "contact_selected" : ""
+                    }
+                  >
+                    Kontakt
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <section>
+            <Routes>
+              <Route path="/" index element={<Home />} />
+              <Route path="/news" element={<News />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </section>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
